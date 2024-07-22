5358b733adc7fd0034c5990879ba4af0fdc3b17b
canutoilberto <canutoilberto@gmail.com>
2024-07-22 09:02:25 -0300

    [Fix] Firebase config fixed

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-export const firebaseApp = initializeApp(firebaseConfig);
-export const db = getFirestore(firebaseApp);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
