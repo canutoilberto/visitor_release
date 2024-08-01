c4a6cbe57a9fa7f116ea3e3a69dd76236e5dab30
canutoilberto <canutoilberto@gmail.com>
2024-08-01 10:50:42 -0300

    [Feat] Logo added

--- src/components/modules/topbar/Topbar.jsx
+++ src/components/modules/topbar/Topbar.jsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useFormStore } from "@/api/formStore";
 import { MdOutlineLogout } from "react-icons/md";
+import Logo from "../../../../public/logo.png";
+import Image from "next/image";
 
 const Topbar = () => {
   const logout = useFormStore((state) => state.logout);
@@ -29,7 +31,12 @@ const Topbar = () => {
   return (
     <header className="w-full bg-background border-b mb-4">
       <div className="container mx-auto max-w-[1200px] px-4 py-3 flex items-center justify-between">
-        <div className="text-lg font-medium">Registro de visitantes</div>
+        <div className="flex items-center justify-center">
+          <Image src={Logo} alt="Logomarca" width={42} height={42} />
+          <div className="text-lg font-semibold ml-2">
+            Registro de visitantes
+          </div>
+        </div>
         <Button
           variant="ghost"
           size="sm"
